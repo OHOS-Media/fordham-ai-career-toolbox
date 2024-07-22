0118b953b737a0c332c330dbc2d83515fdfc0efc
Cass <cassidyjwalters@gmail.com>
2024-07-22 10:23:45 -0700

    feat: add an error alert
    - Create ErrorAlert component
    - Add logic to conditionally render the keywords
    - Add logic for conditionally rendering the ErrorAlert component, and clearing it after 5 seconds
    - Add minimal styling to the Error Alert component

---
+++ components/ErrorAlert.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+export default function ErrorAlert({
+  errorMessage,
+  setErrorMessage,
+  errorAlertActive,
+  setErrorAlertActive,
+}) {
+
+  // Clear the error message after 5s
+  useEffect(() => {
+    async function clearError() {
+      setTimeout(() => {
+        setErrorMessage('');
+        setErrorAlertActive(false);
+      }, 5000);
+    }
+
+    clearError();
+  }, [errorAlertActive]);
+
+  return (
+    <div className="absolute flex justify-center -top-8 left-0 w-full text-red-600">
+      <p>{errorMessage}</p>
+    </div>
+  );
+}
