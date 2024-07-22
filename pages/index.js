965ca476b829b4ec693aff60545c7ce583709c11
Cass <cassidyjwalters@gmail.com>
2024-07-22 10:23:45 -0700

    feat: add an error alert
    - Create ErrorAlert component
    - Add logic to conditionally render the keywords
    - Add logic for conditionally rendering the ErrorAlert component, and clearing it after 5 seconds
    - Add minimal styling to the Error Alert component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,49 @@
 // pages/index.js
-import { useState } from "react";
+import { useState } from 'react';
+import ErrorAlert from '@/components/ErrorAlert';
 
 export default function Home() {
-  const [jobDescription, setJobDescription] = useState("");
+  const [jobDescription, setJobDescription] = useState('');
   const [keywords, setKeywords] = useState([]);
+  const [errorAlertActive, setErrorAlertActive] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("/api/extractKeywords", {
-      method: "POST",
+    const response = await fetch('/api/extractKeywords', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ jobDescription }),
     });
 
     const data = await response.json();
+
+    // If the data is returned with an error,
+    // set the Error Alert to true/active
+    if (data.error) {
+      console.log(data.error);
+      setErrorAlertActive(true);
+      setErrorMessage(data.error);
+    }
+
     setKeywords(data.keywords);
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-96">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="relative bg-white p-8 rounded shadow-md w-96">
+        {/* If the Error Alert is active render it */}
+        {errorAlertActive && (
+          <ErrorAlert
+            errorMessage={errorMessage}
+            setErrorMessage={setErrorMessage}
+            errorAlertActive={errorAlertActive}
+            setErrorAlertActive={setErrorAlertActive}
+          />
+        )}
         <h1 className="text-2xl mb-4">Keyword Extractor</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -41,7 +62,7 @@ export default function Home() {
             Extract Keywords
           </button>
         </form>
-        {keywords.length > 0 && (
+        {keywords?.length > 0 && (
           <div className="mt-4">
             <h2 className="text-xl">Extracted Keywords:</h2>
             <ul className="mt-2 bg-gray-200 p-2 rounded list-disc list-inside">
