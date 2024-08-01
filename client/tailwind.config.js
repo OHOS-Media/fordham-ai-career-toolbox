b232bd1ce93dc144adb646dd6af7f490b8ee0b32
hicass <cassidyjwalters@gmail.com>
2024-08-01 11:24:30 -0700

    Add custom font sizes to the tailwind config that match the design style guidelines, and apply them automatically in the css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
       'error-state': '#EC473C',
       neutral: '#FFFFFF'
     },
+    fontSize: {
+      sm: '0.875rem',
+      base: '1rem',
+      md: '1.125rem',
+      lg: '1.5rem',
+      'h1': '3rem',
+      'h2': '2.5rem',
+      'h3': '2rem',
+      'h4': '1.75rem'
+    },
   },
   plugins: [],
 };
