1551404476298c6095640986be5a41f78d5711a2
hicass <cassidyjwalters@gmail.com>
2024-08-01 11:25:56 -0700

    Add note to refactor button logic to include rendering icons

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,4 +1,5 @@
 export default function Button({ text }) {
+  // TODO: Add logic to dynamically render icons if one is passed as a prop
   return (
     <button className="bg-primary text-neutral font-bold py-2 px-10 rounded-lg">
       {text}
