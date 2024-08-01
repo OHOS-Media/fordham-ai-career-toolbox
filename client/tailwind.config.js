b705d5622dd3bd6b5371e31c5663153662dccb8e
hicass <cassidyjwalters@gmail.com>
2024-07-31 17:20:12 -0700

    Update Tailwind color theme to match Design style guidelines: - Remove default tailwind theme - Update component styles to use new theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: '#473BF0',
+      secondary: '#161C2D',
+      tertiary: '#EAEAFD',
+      'error-state': '#EC473C',
+      neutral: '#FFFFFF'
     },
   },
   plugins: [],
