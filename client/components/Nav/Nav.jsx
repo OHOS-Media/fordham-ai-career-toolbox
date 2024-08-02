7bd07d137aa9058d5ecd160244e5b349d26c268b
hicass <cassidyjwalters@gmail.com>
2024-08-02 12:08:40 -0700

    Move the logic for login in into the nav, and add feature displaying the users profile pic instead of the login button if a user is logged in

--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import LoginPopup from "@/components/LoginPopup";
 import Logo from "../Logo";
 import Button from "../Button";
 import NavLink from "./NavLink";
@@ -12,21 +17,86 @@ const navData = {
 };
 
 export default function Nav() {
+  // Login state
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/user`, {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        console.log("Fetch response:", response);
+        if (response.ok) {
+          const userData = await response.json();
+          console.log("User data:", userData);
+          setUser(userData);
+        } else {
+          const errorData = await response.json();
+          throw new Error(errorData.error || "Failed to fetch user data");
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleLoginLogout = () => {
+    if (isLoggedIn) {
+      // Perform logout logic here
+      setIsLoggedIn(false);
+    } else {
+      // Show login popup
+      setShowLoginPopup(true);
+    }
+  };
+
   return (
     <nav className="absolute top-0 left-0 p-6 flex justify-between w-full">
-      <Logo />
+      <Link href="/">
+        <Logo />
+      </Link>
 
       <div className="flex items-center gap-10">
         {navData.links.map((link, idx) => (
           <NavLink title={link.title} target={link.href} key={idx} />
         ))}
 
-        {/* TODO: Move the logic to log in here */}
-        <Button text={"Log In"} />
+        {user ? (
+          <Link href="/profile" className="w-12 h-12 mr-4 rounded-full overflow-hidden">
+            <Image
+              src={user.profilePicture}
+              alt={user.displayName}
+              width={64}
+              height={64}
+              className="object-cover"
+            />
+          </Link>
+        ) : (
+          <div onClick={handleLoginLogout}>
+            <Button text={"Log In"} />
+          </div>
+        )}
         {/* 
           TODO: Add dynamic profile link/icon if there is a user authenticated
          */}
       </div>
+
+      {/* Popup */}
+      {showLoginPopup && <LoginPopup onClose={() => setShowLoginPopup(false)} />}
     </nav>
   );
 }
