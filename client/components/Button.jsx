0fb3a8449469b8fdd722e4777e7cff6ef72e4791
hicass <cassidyjwalters@gmail.com>
2024-08-05 16:18:16 -0700

    Apply custom tailwind default rounded corners to button

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({ text, onClick, type = "button", className = "" 
     <button
       type={type}
       onClick={onClick}
-      className={`bg-primary text-neutral font-bold py-2 px-10 rounded-lg hover:bg-primary-dark transition-colors ${className}`}
+      className={`bg-primary text-neutral font-bold py-2 px-10 rounded hover:bg-primary-dark transition-colors ${className}`}
     >
       {text}
     </button>
