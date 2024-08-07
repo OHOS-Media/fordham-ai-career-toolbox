e5de3a2f1defec3d15898c2d702df8741b0d32c4
hicass <cassidyjwalters@gmail.com>
2024-08-07 10:24:48 -0700

    Remove un-used semi colon

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
