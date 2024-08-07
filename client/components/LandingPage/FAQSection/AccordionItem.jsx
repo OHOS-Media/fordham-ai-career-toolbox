ce543d7c13885bfb56eecf307b4cc72290638501
hicass <cassidyjwalters@gmail.com>
2024-08-07 10:26:01 -0700

    Create an FAQ section, style it, and add it to the Landing Page

---
+++ client/components/LandingPage/FAQSection/AccordionItem.jsx
@@ -0,0 +1,40 @@
+import { useState, useRef, useEffect } from "react";
+import Image from "next/image";
+import TriangleArrow from "@/public/assets/TriangleArrow.svg";
+
+const AccordionItem = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const answerRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      answerRef.current.style.maxHeight = `${answerRef.current.scrollHeight}px`;
+    } else {
+      answerRef.current.style.maxHeight = "0px";
+    }
+  }, [isOpen]);
+
+  return (
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      className="m-4 p-4 hover:cursor-pointer border border-tertiary"
+    >
+      <div className="flex justify-between">
+        <p className="text-md font-bold ">{question}</p>
+        <Image
+          src={TriangleArrow}
+          width={22}
+          height={22}
+          className={isOpen ? "rotate-180" : ""}
+          alt="Triangle Arrow"
+        />
+      </div>
+
+      <div className={`accordion-answer ${isOpen ? "open" : ""}`} ref={answerRef}>
+        <p>{answer}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItem;
