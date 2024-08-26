19a4cf35702a92415e8f5141b5ddf35c6fb649c4
hicass <cassidyjwalters@gmail.com>
2024-08-26 12:58:34 -0700

    Update Toolbox Step 1 to match the wirerame

--- client/components/ToolboxPage/ToolboxStep1.jsx
+++ client/components/ToolboxPage/ToolboxStep1.jsx
@@ -1,3 +1,5 @@
+import Button from "../Button";
+
 const toolboxStep1FormData = {
   placeholderText:
     "Please paste the full job description here so we can analyze its required skills. To ensure accuracy, include sections detailing roles, responsibilities, and qualifications. Exclude sections like 'About Us/Company,' salary/benefits, and diversity statements, as they typically don't mention hard skills. Only English job descriptions, please.",
@@ -8,23 +10,15 @@ export default function ToolboxStep1({ handleSubmit, jobDescription, setJobDescr
     <div className="flex flex-col gap-6">
       <h4 className="h4">Paste a job description below...</h4>
 
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <textarea
-            className="w-full p-6 rounded overflow-scroll"
-            rows="10"
-            placeholder={toolboxStep1FormData.placeholderText}
-            value={jobDescription}
-            onChange={(e) => setJobDescription(e.target.value)}
-          ></textarea>
-        </div>
-        <button
-          className="w-full bg-primary text-neutral p-2 rounded"
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Extracting..." : "Extract Keywords"}
-        </button>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center w-full">
+        <textarea
+          className="w-full p-6 rounded overflow-scroll mb-6"
+          rows="10"
+          placeholder={toolboxStep1FormData.placeholderText}
+          value={jobDescription}
+          onChange={(e) => setJobDescription(e.target.value)}
+        ></textarea>
+        <Button type="submit" text={loading ? "Extracting..." : "Submit"} disabled={loading} />
       </form>
     </div>
   );
