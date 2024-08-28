41c9e543ed618f152c05cad2f3e287293de81a47
Cass <cassidyjwalters@gmail.com>
2024-08-28 10:24:57 -0700

    refactor: Update Steps Section on Landing Page (#23)
    * Update the Arrow SVG to match adjusted one on wireframe
    
    * Update steps section to have the new addtional step

--- client/components/LandingPage/StepsSection/StepsSection.jsx
+++ client/components/LandingPage/StepsSection/StepsSection.jsx
@@ -2,32 +2,38 @@ import StepCard from "./StepCard";
 import Arrow from "./Arrow";
 
 const stepSectionData = {
-  title: "Get a Polished Resume in 3 Easy Steps",
+  title: "Get a Polished Resume in 4 Easy Steps",
   step1: {
     number: "1",
-    text: "Upload your resume",
+    text: "Copy and paste a desired job’s description",
   },
   step2: {
     number: "2",
-    text: "Copy and paste a desired job’s description",
+    text: "Get AI recommended keywords",
   },
   step3: {
     number: "3",
-    text: "Apply the AI suggested changes to your resume!",
+    text: "Copy and paste your resume",
+  },
+  step4: {
+    number: "4",
+    text: "Get resume bullet point suggestions and add them to your resume!",
   },
 };
 
 const StepsSection = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-grey py-16 px-28 rounded-lg">
+    <div className="flex flex-col items-center justify-center w-4/5 bg-grey py-16 px-10 rounded-lg">
       <h2 className="h2">{stepSectionData.title}</h2>
 
-      <div className="flex justify-center mt-10">
+      <div className="flex justify-center items-center mt-10">
         <StepCard number={stepSectionData.step1.number} text={stepSectionData.step1.text} />
-        <Arrow margin={"mt-8 mx-4"} />
+        <Arrow margin={"mx-4"} />
         <StepCard number={stepSectionData.step2.number} text={stepSectionData.step2.text} />
-        <Arrow margin={"mt-8 mx-4"} />
+        <Arrow margin={"mx-4"} />
         <StepCard number={stepSectionData.step3.number} text={stepSectionData.step3.text} />
+        <Arrow margin={"mx-4"} />
+        <StepCard number={stepSectionData.step4.number} text={stepSectionData.step4.text} />
       </div>
     </div>
   );
