30bcb6dce09b3dcde583e9859753fc099a7dcba9
hicass <cassidyjwalters@gmail.com>
2024-08-29 11:36:41 -0700

    Create a reusable Toolbox Form component to ensure consistency for toolbox steps 1 and 3

--- client/components/ToolboxPage/ToolboxStep1.jsx
+++ client/components/ToolboxPage/ToolboxStep1.jsx
@@ -1,29 +1,19 @@
-import Button from "../Button";
+import ToolboxForm from "./ToolboxForm";
 
 const toolboxStep1FormData = {
+  title: "Paste a job description below...",
   placeholderText:
     "Please paste the full job description here so we can analyze its required skills. To ensure accuracy, include sections detailing roles, responsibilities, and qualifications. Exclude sections like 'About Us/Company,' salary/benefits, and diversity statements, as they typically don't mention hard skills. Only English job descriptions, please.",
 };
 
 export default function ToolboxStep1({ handleSubmit, jobDescription, setJobDescription, loading }) {
   return (
-    <div className="flex flex-col gap-4 h-full">
-      <h4 className="h4">Paste a job description below...</h4>
-
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center w-full h-full justify-between"
-      >
-        <textarea
-          className="w-full p-6 rounded overflow-scroll mb-4"
-          rows="11"
-          placeholder={toolboxStep1FormData.placeholderText}
-          value={jobDescription}
-          onChange={(e) => setJobDescription(e.target.value)}
-        ></textarea>
-
-        <Button type="submit" text={loading ? "Extracting..." : "Submit"} disabled={loading} />
-      </form>
-    </div>
+    <ToolboxForm
+      formData={toolboxStep1FormData}
+      handleSubmit={handleSubmit}
+      value={jobDescription}
+      onChange={(e) => setJobDescription(e.target.value)}
+      loading={loading}
+    />
   );
 }
--- client/components/ToolboxPage/ToolboxStep3.jsx
+++ client/components/ToolboxPage/ToolboxStep3.jsx
@@ -1,3 +1,19 @@
-export default function ToolboxStep3() {
-  return <h1 className="h1">Toolbox Step 3</h1>;
+import ToolboxForm from "./ToolboxForm";
+
+const toolboxStep3FormData = {
+  title: "Paste your resume",
+  placeholderText:
+    "Please paste the full resume here so we can analyze the skills and experience listed. To ensure accuracy, include sections detailing your work history, job titles, responsibilities, and education. Exclude sections like personal statements, hobbies, or references, as they typically don't highlight specific skills. Only resumes in English, please.",
+};
+
+export default function ToolboxStep3({ handleSubmitResume, resume, setResume, loading }) {
+  return (
+    <ToolboxForm
+      formData={toolboxStep3FormData}
+      handleSubmit={handleSubmitResume}
+      value={resume}
+      onChange={(e) => setResume(e.target.value)}
+      loading={loading}
+    />
+  );
 }
