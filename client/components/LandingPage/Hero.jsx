0fc2f42e65bdc01a55408115f272ba651bd233cd
Bach Le <bachle@Bachs-MacBook-Pro.local>
2024-09-20 00:16:29 +0700

    fix

--- client/components/LandingPage/Hero.jsx
+++ client/components/LandingPage/Hero.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Hero = ({ title, description, ctaText, ctaLink }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-neutral text-center px-4 mt-32">
+    <div className="flex flex-col items-center justify-center text-center px-4 mt-32">
       <h1 className="h1 mb-6">{title}</h1>
       <p className="body-txt-md opacity-70 mb-8 max-w-2xl">{description}</p>
       <Link href={ctaLink}>
