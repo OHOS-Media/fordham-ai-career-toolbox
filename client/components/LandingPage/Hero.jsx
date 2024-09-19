83949c6ece1e805376a85f713e74f84170c21b7c
Bach Le <bachle@Bachs-MacBook-Pro.local>
2024-09-20 03:42:27 +0700

    resolve hero

--- client/components/LandingPage/Hero.jsx
+++ client/components/LandingPage/Hero.jsx
@@ -4,13 +4,6 @@ import Link from "next/link";
 
 const Hero = ({ title, description, ctaText, ctaLink }) => {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col items-center justify-center text-center px-4 mt-32">
-      <h1 className="h1 mb-6">{title}</h1>
-      <p className="body-txt-md opacity-70 mb-8 max-w-2xl">{description}</p>
-      <Link href={ctaLink}>
-        <Button text={ctaText} />
-=======
     <div className="flex flex-col items-center justify-center text-center px-4 mt-48">
       <h1 className="h1 mb-6">
         Introducing{" "}
@@ -21,7 +14,6 @@ const Hero = ({ title, description, ctaText, ctaLink }) => {
       </p>
       <Link href="/toolbox">
         <Button text="Try our Tool" />
->>>>>>> 6e6a67ad589b7573fb8a27315aa430e8e177dd71
       </Link>
     </div>
   );
