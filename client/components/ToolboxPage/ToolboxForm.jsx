ac91019e4aac80155c691869ccfa06f858516bd2
hicass <cassidyjwalters@gmail.com>
2024-09-19 13:05:17 -0700

    Update the colors and style of the ToolboxForm component to match the new design wireframe

--- client/components/ToolboxPage/ToolboxForm.jsx
+++ client/components/ToolboxPage/ToolboxForm.jsx
@@ -2,16 +2,16 @@ import Button from "../Button";
 
 export default function ToolboxForm({ formData, handleSubmit, value, onChange, loading }) {
   return (
-    <div className="flex flex-col gap-4 h-full">
-      <h4 className="h4">{formData.title}</h4>
+    <div className="flex flex-col justify-between gap-4 h-full">
+      <h4 className="body-txt-lg-bold text-secondary">{formData.title}</h4>
 
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col items-center w-full h-full justify-between"
+        className="flex flex-col items-end w-full h-full justify-between"
       >
         <textarea
-          className="w-full p-6 rounded overflow-scroll mb-4"
-          rows="11"
+          className="w-full p-6 rounded overflow-scroll outline-primary/20"
+          rows="10"
           placeholder={formData.placeholderText}
           value={value}
           onChange={onChange}
