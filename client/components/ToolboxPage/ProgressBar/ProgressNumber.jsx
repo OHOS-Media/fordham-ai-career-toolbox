7db82f51c6fbbd4a0db60c6cc61d70d8df25ede6
hicass <cassidyjwalters@gmail.com>
2024-10-07 16:35:42 -0700

    Update Progress Bar components to look better across devices

--- client/components/ToolboxPage/ProgressBar/ProgressNumber.jsx
+++ client/components/ToolboxPage/ProgressBar/ProgressNumber.jsx
@@ -15,17 +15,17 @@ export default function ProgressNumber({ step, activeStep }) {
 
   return (
     <div className="flex flex-col gap-2 relative items-center w-full">
-      <div className={`${applyStyles()} flex justify-center items-center w-14 h-14 rounded-full`}>
+      <div className={`${applyStyles()} flex justify-center items-center w-10 h-10 sm:w-14 sm:h-14 rounded-full`}>
         <p className="body-txt-md-bold">{step.number}</p>
       </div>
 
       {step.number < 4 && (
-        <div className="absolute top-5 -right-16">
+        <div className="absolute w-[4rem] sm:w-[6rem] md:w-[7rem] lg:w-[8rem] top-5 -right-8 sm:-right-12 md:-right-14 lg:-right-16">
           <Arrow step={step} activeStep={activeStep} />
         </div>
       )}
 
-      <p className="body-txt text-secondary opacity-70">{step.description}</p>
+      <p className="body-txt text-secondary text-center opacity-70">{step.description}</p>
     </div>
   );
 }
