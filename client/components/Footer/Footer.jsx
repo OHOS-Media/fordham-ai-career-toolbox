676362b34d1f6122e5f331e3e1802b4f18e84708
hicass <cassidyjwalters@gmail.com>
2024-10-08 14:55:07 -0700

    Change Footer logo to a temporary placeholder, and update styling to be more responsive

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -11,7 +11,8 @@ const footerData = {
   links: [
     { title: "Toolbox", href: "/toolbox" },
     { title: "About Us", href: "/about-us" },
-    { title: "Wordpress Link", href: "/" },
+    { title: "Blog", href: "/Blog" },
+    { title: "F.A.Q", href: "/faq" },
   ],
   socialLinks: [
     { title: "Twitter/X", href: "/", icon: TwitterLogo },
@@ -24,23 +25,23 @@ const footerData = {
 const Footer = () => {
   return (
     <footer className="flex justify-center py-16 bg-primary w-full text-white">
-      <div className="flex flex-col w-1/5 gap-8">
-        <div className="w-fit">
-          <Logo />
-        </div>
-        <p className="body-txt color-secondary opacity-70">{footerData.description}</p>
+      <div className="flex flex-col-reverse lg:flex-row w-11/12 lg:w-5/6 gap-6 lg:gap-0">
+        <div className="flex flex-col lg:w-1/3 gap-4">
+          <h3 className="h3">Fordham University</h3>
+          <p className="body-txt color-secondary opacity-70">{footerData.description}</p>
 
-        <div className="flex gap-8">
-          {footerData.socialLinks.map((link, idx) => (
-            <SocialIconLink title={link.title} target={link.href} icon={link.icon} key={idx} />
+          <div className="flex gap-8">
+            {footerData.socialLinks.map((link, idx) => (
+              <SocialIconLink title={link.title} target={link.href} icon={link.icon} key={idx} />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col sm:flex-row w-fit lg:w-2/3 gap-8 lg:justify-evenly">
+          {footerData.links.map((link, idx) => (
+            <FooterLink title={link.title} target={link.href} key={idx} />
           ))}
         </div>
-      </div>
-
-      <div className="flex w-1/2 justify-evenly">
-        {footerData.links.map((link, idx) => (
-          <FooterLink title={link.title} target={link.href} key={idx} />
-        ))}
       </div>
     </footer>
   );
