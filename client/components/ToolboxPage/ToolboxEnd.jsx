b935d0c2768ccfd12f6e4bf900e4c53e3737c79f
hicass <cassidyjwalters@gmail.com>
2024-10-08 15:34:38 -0700

    Add functionality to the Toolbox End component where it displays a randomized motivating message and then redirects to the home page

--- client/components/ToolboxPage/ToolboxEnd.jsx
+++ client/components/ToolboxPage/ToolboxEnd.jsx
@@ -1,3 +1,37 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const messages = [
+  "Go make an impact!",
+  "You're capable. Own it!",
+  "Show the world your skills!",
+  "Success is within reach!",
+  "You're ready to shine!",
+  "You've got this!",
+  "Chase your dreams!",
+  "Take the first step!",
+  "Own your future!",
+  "Go make a difference!",
+];
+
 export default function ToolboxEnd() {
-  return <h1 className="h1">Toolbox End!</h1>;
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    function messageRandomizer() {
+      const randomIndex = Math.floor(Math.random() * messages.length);
+      setMessage(messages[randomIndex]);
+    }
+
+    messageRandomizer();
+
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return <h1 className="h1 text-primary">{message}</h1>;
 }
