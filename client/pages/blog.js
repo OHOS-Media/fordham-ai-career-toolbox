60ccc3e64116a915354850443c83a28a18c9cc74
hicass <cassidyjwalters@gmail.com>
2024-10-15 14:40:06 -0700

    Set up Blog Page

---
+++ client/pages/blog.js
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center bg-neutral relative w-full border-primary pt-32">
+      <h1 className="h1 text-secondary">Blogs</h1>
+    </div>
+  )
+}
