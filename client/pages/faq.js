fc522a4bd5888a418b335aaa0d99432ab27e2a66
hicass <cassidyjwalters@gmail.com>
2024-10-15 15:26:09 -0700

    Update styling of the FAQ page

--- client/pages/faq.js
+++ client/pages/faq.js
@@ -2,7 +2,7 @@ import FAQSection from "@/components/FAQPage/FAQSection";
 
 export default function FAQ() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center bg-neutral relative w-full border-primary pt-36">
+    <div className="min-h-screen flex flex-col gap-10 items-center bg-neutral w-full pt-36">
       <h1 className="h2 text-secondary">Frequently Asked Questions</h1>
       <FAQSection />
     </div>
