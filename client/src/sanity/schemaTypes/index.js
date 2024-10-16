80b0acd9c4d3d3149336f3a14e5bdb5acc7f69c8
hicass <cassidyjwalters@gmail.com>
2024-10-16 11:47:57 -0700

    Remove unneeded categoryType

--- client/src/sanity/schemaTypes/index.js
+++ client/src/sanity/schemaTypes/index.js
@@ -1,9 +1,8 @@
 import { blockContentType } from "./blockContentType";
-import { categoryType } from "./categoryType";
 import { blogType } from "./blogType";
 import { authorType } from "./authorType";
 import { homePageType } from "./homePageType";
 
 export const schema = {
-  types: [blockContentType, categoryType, blogType, authorType, homePageType],
+  types: [blockContentType, blogType, authorType, homePageType],
 };
