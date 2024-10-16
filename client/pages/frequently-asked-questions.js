1f24519b82f6680d50c1b7e6f5c6cd7a07eb065a
hicass <cassidyjwalters@gmail.com>
2024-10-16 15:11:53 -0700

    Update faq -> frequently asked questions

---
+++ client/pages/frequently-asked-questions.js
@@ -0,0 +1,23 @@
+import FAQSection from "@/components/FAQPage/FAQSection";
+import { client } from "@/src/sanity/lib/client";
+
+export default function FAQ({ FAQPageData }) {
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center bg-neutral w-full pt-36">
+      <h1 className="h2 text-secondary">Frequently Asked Questions</h1>
+      <FAQSection FAQPageData={FAQPageData} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const FAQPageData = await client.fetch("*[_type == 'faq']");
+  console.log(FAQPageData);
+
+  return {
+    props: {
+      FAQPageData,
+    },
+    revalidate: 30, // Revalidate every 30 seconds
+  };
+}
