5d702cc0971cc1047a408445a28e35a6e52b5a36
hicass <cassidyjwalters@gmail.com>
2024-10-16 15:11:30 -0700

    Update FAQ components to use data from Sanity

--- client/components/FAQPage/FAQSection.jsx
+++ client/components/FAQPage/FAQSection.jsx
@@ -1,28 +1,10 @@
 import AccordionItem from "./AccordionItem";
 
-const faqSectionData = [
-  {
-    question: "Lorem ipsum?",
-    answer:
-      "Aliquam fusce hac purus fames id tincidunt. Sed augue nostra; parturient integer mauris at. Viverra scelerisque habitasse molestie erat primis dis cras laoreet. Facilisis rutrum nisi class bibendum morbi vivamus inceptos ex.",
-  },
-  {
-    question: "Lorem ipsum?",
-    answer:
-      "Aliquam fusce hac purus fames id tincidunt. Sed augue nostra; parturient integer mauris at. Viverra scelerisque habitasse molestie erat primis dis cras laoreet. Facilisis rutrum nisi class bibendum morbi vivamus inceptos ex.",
-  },
-  {
-    question: "Lorem ipsum?",
-    answer:
-      "Aliquam fusce hac purus fames id tincidunt. Sed augue nostra; parturient integer mauris at. Viverra scelerisque habitasse molestie erat primis dis cras laoreet. Facilisis rutrum nisi class bibendum morbi vivamus inceptos ex.",
-  },
-];
-
-const FAQSection = () => {
+const FAQSection = ({ FAQPageData }) => {
   return (
     <div className="flex flex-col gap-6 items-center md:w-2/3 lg:w-1/2 2xl:w-1/3 mb-60">
       <div className="flex flex-col w-full">
-        {faqSectionData.map((faq, idx) => (
+        {FAQPageData.map((faq, idx) => (
           <AccordionItem question={faq.question} answer={faq.answer} key={idx} />
         ))}
       </div>
