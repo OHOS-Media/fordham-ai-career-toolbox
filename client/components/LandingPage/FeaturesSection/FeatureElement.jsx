0436b1a7ad7d1aaf89ba99c5c6fbfc33c05dd422
hicass <cassidyjwalters@gmail.com>
2024-10-20 10:27:10 -0700

    Add drop shadow to Feature Element

--- client/components/LandingPage/FeaturesSection/FeatureElement.jsx
+++ client/components/LandingPage/FeaturesSection/FeatureElement.jsx
@@ -2,10 +2,10 @@ import FeatureIcon from "./FeatureIcon";
 
 const FeatureElement = ({ feature }) => {
   return (
-    <div className="flex flex-col items-center md:w-1/3 lg:w-1/5 mx-4 gap-4 md:gap-1">
+    <div className="flex flex-col items-center md:w-1/3 lg:w-1/5 mx-4 gap-4 md:gap-1 shadow-sm">
       <FeatureIcon icon={feature.icon} iconAlt={feature.iconAlt} />
-      <p className="body-txt-bold text-center">{feature.title}</p>
-      <p className="body-txt text-center">{feature.description}</p>
+      <p className="body-txt-bold text-center text-secondary">{feature.title}</p>
+      <p className="body-txt text-center text-secondary">{feature.description}</p>
     </div>
   );
 };
