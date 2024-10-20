375dcff5670bca5b26e96d971f379d2efc1b3232
hicass <cassidyjwalters@gmail.com>
2024-10-20 10:27:44 -0700

    Add drop shadow to keyword element

--- client/components/ToolboxPage/Keyword.jsx
+++ client/components/ToolboxPage/Keyword.jsx
@@ -1,6 +1,6 @@
 export default function Keyword({ word }) {
   return (
-    <div className="bg-dark-grey w-fit rounded p-[.5em] text-neutral body-txt text-white">
+    <div className="bg-dark-grey shadow-sm w-fit rounded p-[.5em] text-neutral body-txt text-white">
       <p>{word}</p>
     </div>
   );
