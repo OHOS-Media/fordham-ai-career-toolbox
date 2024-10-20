986ec245146f88e7d0c8562cc477f8931c4b37c6
hicass <cassidyjwalters@gmail.com>
2024-10-20 10:36:25 -0700

    Fix feature icon drop shadows

--- client/components/LandingPage/FeaturesSection/FeatureElement.jsx
+++ client/components/LandingPage/FeaturesSection/FeatureElement.jsx
@@ -2,7 +2,7 @@ import FeatureIcon from "./FeatureIcon";
 
 const FeatureElement = ({ feature }) => {
   return (
-    <div className="flex flex-col items-center md:w-1/3 lg:w-1/5 mx-4 gap-4 md:gap-1 shadow-sm">
+    <div className="flex flex-col items-center md:w-1/3 lg:w-1/5 mx-4 gap-4 md:gap-1">
       <FeatureIcon icon={feature.icon} iconAlt={feature.iconAlt} />
       <p className="body-txt-bold text-center text-secondary">{feature.title}</p>
       <p className="body-txt text-center text-secondary">{feature.description}</p>
