89e82b36b820619e7b7f5678b21fb9d429cda5ea
hicass <cassidyjwalters@gmail.com>
2024-10-20 10:00:07 -0700

    Update Blog Page styling

--- client/pages/blog.js
+++ client/pages/blog.js
@@ -3,7 +3,7 @@ import { client } from "@/src/sanity/lib/client";
 
 export default function Blog({ BlogPageData }) {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center bg-neutral relative w-full border-primary pt-36">
+    <div className="min-h-screen flex flex-col gap-10 bg-neutral items-center w-full py-36">
       <h1 className="h1 text-secondary">Blogs</h1>
 
       <BlogsPreview BlogPageData={BlogPageData} />
@@ -12,7 +12,15 @@ export default function Blog({ BlogPageData }) {
 }
 
 export async function getStaticProps() {
-  const BlogPageData = await client.fetch("*[_type == 'blog']");
+  const BlogPageData = await client.fetch(`
+    *[_type == 'blog']{
+      title,
+      "body": array::join(string::split(pt::text(body), "")[0..200], "") + "...",
+      slug,
+      mainImage
+    }
+  `);
+  console.log(BlogPageData);
 
   return {
     props: {
