fcfaff2b7f2d8b04bd78e0d81ce0cb2569792003
Bach Le <bachle@Bachs-MacBook-Pro.local>
2024-10-21 13:40:46 +0700

    fix deploy

--- client/components/LandingPage/TestimonialsSection/TestimonialsSection.jsx
+++ client/components/LandingPage/TestimonialsSection/TestimonialsSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Testimonial = ({ quote, text, name, title }) => (
-  <div className="flex flex-col items-center text-center">
-    <h3 className="text-2xl font-bold mb-6"> `&quot;`{quote}`&quot;`</h3>
+  <div className="flex flex-col items-center text-center max-w-md mx-auto">
+    <h3 className="text-2xl font-bold mb-6">&quot;{quote}&quot;</h3>
     <p className="text-sm mb-6 opacity-80">{text}</p>
     <p className="font-semibold">{name}</p>
     <p className="text-sm opacity-80">{title}</p>
@@ -16,13 +16,12 @@ const TestimonialsSection = ({ testimonials }) => {
 
   return (
     <section className="bg-primary w-full text-white py-40">
-      {/* <h2 className="text-3xl sm:text-4xl font-bold text-center text-secondary mb-8 sm:mb-12">
-      Testimonials
-    </h2> */}
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="flex flex-wrap justify-center gap-20">
           {testimonials.map((testimonial, index) => (
-            <Testimonial key={index} {...testimonial} />
+            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
+              <Testimonial {...testimonial} />
+            </div>
           ))}
         </div>
       </div>
