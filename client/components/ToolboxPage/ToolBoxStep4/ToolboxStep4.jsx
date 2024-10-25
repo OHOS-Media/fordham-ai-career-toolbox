2c6a3eda7117f2c7e099b849e2f3e86885d95c2e
hicass <cassidyjwalters@gmail.com>
2024-10-25 15:43:32 -0700

    Cleanup div's and remove unneeded tailwind rules of the Toolbox Step 2 and 4

--- client/components/ToolboxPage/ToolBoxStep4/ToolboxStep4.jsx
+++ client/components/ToolboxPage/ToolBoxStep4/ToolboxStep4.jsx
@@ -5,23 +5,21 @@ import Button from "../../Button";
 export default function ToolboxStep4({ bulletPoints, incrementStep }) {
   return (
     <div className="flex flex-col gap-4 justify-between h-full">
-      <div className="h-full">
-        <h4 className="body-txt-lg-bold text-secondary px-4">Get Bullet Points</h4>
+      <h4 className="body-txt-lg-bold text-secondary px-4">Get Bullet Points</h4>
 
-        <div className="flex flex-col gap-4 w-fit mt-4 h-full relative">
-          {/* Map through bulletPoints array to render each job's bullet points */}
-          {bulletPoints.map((job, jobIndex) =>
-            // For each job, map through its key-value pairs to render BulletItem components
-            Object.entries(job).map(([jobTitle, bulletPoint], idx) => (
-              <BulletItem
-                jobTitle={jobTitle}
-                bulletPoint={bulletPoint}
-                jobIndex={jobIndex}
-                key={idx}
-              />
-            ))
-          )}
-        </div>
+      <div className="flex flex-col gap-4 w-full mt-4 h-full relative">
+        {/* Map through bulletPoints array to render each job's bullet points */}
+        {bulletPoints.map((job, jobIndex) =>
+          // For each job, map through its key-value pairs to render BulletItem components
+          Object.entries(job).map(([jobTitle, bulletPoint], idx) => (
+            <BulletItem
+              jobTitle={jobTitle}
+              bulletPoint={bulletPoint}
+              jobIndex={jobIndex}
+              key={idx}
+            />
+          ))
+        )}
       </div>
 
       <div className="flex flex-col sm:flex-row gap-2 sm:gap-0 w-fit sm:w-full justify-between z-30">
--- client/components/ToolboxPage/ToolboxStep2.jsx
+++ client/components/ToolboxPage/ToolboxStep2.jsx
@@ -5,7 +5,7 @@ import Keyword from "./Keyword";
 // This renders Step 2 of the Toolbox which displays the AI reccomended resume keywords
 export default function ToolboxStep2({ keywords, setExitModalActive, incrementStep }) {
   const [showAll, setShowAll] = useState(false);
-  const initialKeywordCount = 10; 
+  const initialKeywordCount = 10;
 
   const handleShowAll = () => {
     setShowAll(!showAll);
@@ -17,25 +17,23 @@ export default function ToolboxStep2({ keywords, setExitModalActive, incrementSt
 
   return (
     <div className="flex flex-col gap-2 justify-between h-full">
-      <div className="flex flex-col">
-        <h4 className="body-txt-lg-bold text-secondary">Get Keywords</h4>
+      <h4 className="body-txt-lg-bold text-secondary">Get Keywords</h4>
 
-        <div className="flex flex-wrap gap-2 mt-4">
-          {/* If showAll is true, render all of the keywords, otherwise only display the initial count */}
-          {keywords.slice(0, showAll ? keywords.length : initialKeywordCount).map((word, idx) => (
-            <Keyword word={word} key={idx} />
-          ))}
+      <div className="flex flex-wrap gap-2 mt-4">
+        {/* If showAll is true, render all of the keywords, otherwise only display the initial count */}
+        {keywords.slice(0, showAll ? keywords.length : initialKeywordCount).map((word, idx) => (
+          <Keyword word={word} key={idx} />
+        ))}
 
-          <button
-            className="bg-neutral p-[.5em] rounded body-txt hover:text-primary"
-            onClick={handleShowAll}
-          >
-            {showAll ? "Show Less" : "Show More"}
-          </button>
-        </div>
+        <button
+          className="bg-neutral p-[.5em] rounded body-txt hover:text-primary"
+          onClick={handleShowAll}
+        >
+          {showAll ? "Show Less" : "Show More"}
+        </button>
       </div>
 
-      <div className="flex flex-col sm:flex-row gap-2 w-fit sm:w-full justify-between">
+      <div className="flex flex-col sm:flex-row gap-2 w-fit sm:w-full justify-between mt-4">
         <Button text={"I'm done"} onClick={openExitModal} className="w-fit" />
         <Button text={"Continue to bullet points..."} onClick={incrementStep} />
       </div>
