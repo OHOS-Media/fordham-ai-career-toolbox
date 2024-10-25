98c54d93a66305aa67a6d04e94a0fadcbe1d07c3
hicass <cassidyjwalters@gmail.com>
2024-10-25 10:42:38 -0700

    Update the PageContainer component to have a boolean prop that determines if the width of the page should be full or limited

--- client/components/PageContainer.jsx
+++ client/components/PageContainer.jsx
@@ -1,5 +1,9 @@
-const PageContainer = ({ children, className = "" }) => {
-  return <div className={`max-w-6xl mx-auto px-4 w-full ${className}`}>{children}</div>;
+const PageContainer = ({ children, className = "", limitedWidth = true }) => {
+  return (
+    <div className={`w-full mx-auto ${limitedWidth && "max-w-6xl px-10"} ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default PageContainer;
