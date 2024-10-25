6682acc5ea9ce899a1c6604b78e60fed798be033
hicass <cassidyjwalters@gmail.com>
2024-10-25 13:59:06 -0700

    Set a top margin for the Page Container that accounts for the space the Nav takes up

--- client/components/PageContainer.jsx
+++ client/components/PageContainer.jsx
@@ -1,6 +1,6 @@
 const PageContainer = ({ children, className = "", limitedWidth = true }) => {
   return (
-    <div className={`w-full mx-auto ${limitedWidth && "max-w-6xl px-10"} ${className}`}>
+    <div className={`w-full mx-auto mt-24  ${limitedWidth && "max-w-6xl px-10"} ${className}`}>
       {children}
     </div>
   );
