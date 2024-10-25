96c58024b57466423917ef49c0ab09c91362c46a
hicass <cassidyjwalters@gmail.com>
2024-10-25 11:11:44 -0700

    Update the styling of the Nav and Logo

--- client/components/Nav/WideNav.jsx
+++ client/components/Nav/WideNav.jsx
@@ -7,20 +7,22 @@ import ErrorAlert from "../ErrorAlert";
 
 export default function WideNav({ navData, isAuthenticated, login, user, error, setError }) {
   return (
-    <nav className="mt-4 pl-8 pr-5 py-2 flex sticky top-5 justify-between shadow-md w-11/12 lg:w-2/3 border-[1px] border-white rounded-full bg-white/50 z-20">
+    <nav className="w-full max-w-5xl w-2/3 flex sticky top-5 justify-between px-6 py-2 shadow-md border-[1px] border-white rounded-full bg-white/50 backdrop-blur-md z-20">
       <div className="flex items-center gap-10">
         <Logo />
       </div>
 
-      <div className="flex items-center gap-10">
+      <div className="flex items-center gap-12">
         {navData.links.map((link, idx) => (
           <NavLink
             title={link.title}
             target={link.href}
-            className="body-txt-bold text-secondary hover:text-primary"
+            className="body-txt-md-bold text-secondary hover:text-primary"
             key={idx}
           />
         ))}
+        
+        {/* Uncomment the lines below to enable authentication */}
         {/* {isAuthenticated ? (
           <Link href="/profile">
             <div className="w-12 h-12 rounded-full overflow-hidden">
