55842634e353f7a012e2c367fcb0ab1f118973c1
hicass <cassidyjwalters@gmail.com>
2024-10-25 16:01:51 -0700

    Cleanup the Blog Page tailwind rules and apply the PageContainer component

--- client/components/BlogPage/BlogsPreview.jsx
+++ client/components/BlogPage/BlogsPreview.jsx
@@ -3,7 +3,7 @@ import BlogCard from "../LandingPage/BlogSection/BlogCard";
 export default function BlogsPreview({ BlogPageData }) {
   if (BlogPageData && BlogPageData.length > 0) {
     return (
-      <section className="py-12 sm:py-16 w-full">
+      <section className="w-full">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
             {BlogPageData.map((blog, idx) => (
