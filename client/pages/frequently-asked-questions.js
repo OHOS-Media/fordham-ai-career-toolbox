3a4b2ce95382ab328afe0e4313d70db6c80e884c
hicass <hicass.dev@gmail.com>
2024-11-11 16:04:43 -0800

    Update FAQ page to use new black background

--- client/pages/frequently-asked-questions.js
+++ client/pages/frequently-asked-questions.js
@@ -5,7 +5,7 @@ import { client } from "@/src/sanity/lib/client";
 export default function FAQ({ FAQPageData }) {
   return (
     <PageContainer>
-      <div className=" w-full flex flex-col items-center gap-10 g-neutral">
+      <div className=" w-full flex flex-col items-center gap-10">
         <h1 className="h2 text-secondary">Frequently Asked Questions</h1>
         <FAQSection FAQPageData={FAQPageData} />
       </div>
