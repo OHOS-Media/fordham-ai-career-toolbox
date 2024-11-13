d71fdf473dd95ac241067d51112134f3d9d4e8c8
Toni <pasotoni03@gmail.com>
2024-11-13 17:58:30 -0500

    Updated Feature Section: - Changed the images with Sanity - Added a border around text - Made changes to the text to be aligned from the left - Added white color to the test

--- client/components/LandingPage/FeaturesSection/FeatureElement.jsx
+++ client/components/LandingPage/FeaturesSection/FeatureElement.jsx
@@ -2,10 +2,10 @@ import FeatureIcon from "./FeatureIcon";
 
 const FeatureElement = ({ feature }) => {
   return (
-    <div className="flex flex-col items-center md:w-1/3 lg:w-1/5 mx-4 gap-4 md:gap-1">
+    <div className="flex flex-col items-start p-6 bg-white/5 rounded-lg shadow-md min-w-[250px] max-w-[300px] h-auto">
       <FeatureIcon icon={feature.icon} iconAlt={feature.iconAlt} />
-      <p className="body-txt-bold text-center text-secondary">{feature.title}</p>
-      <p className="body-txt text-center text-secondary">{feature.description}</p>
+      <p className="body-txt-bold text-left text-white text-xl mt-2 mb-2 w-full overflow-hidden text-ellipsis whitespace-nowrap">{feature.title}</p>
+      <p className="body-txt text-left text-white text-sm mt-auto">{feature.description}</p>
     </div>
   );
 };
--- client/components/LandingPage/FeaturesSection/FeaturesSection.jsx
+++ client/components/LandingPage/FeaturesSection/FeaturesSection.jsx
@@ -2,12 +2,12 @@ import FeatureElement from "./FeatureElement";
 
 const FeatureSection = ({ title, featureItems }) => {
   return (
-    <div className="flex flex-col items-center justify-center z-10">
-      <h2 className="h3 text-center text-fordham-white">{title}</h2>
+    <div className="flex flex-col items-center justify-center w-full sm:w-4/5 py-10 px-10 z-10">
+      <h2 className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center text-white md:whitespace-normal lg:whitespace-nowrap mb-8 sm:mb-12">{title}</h2>
 
-      <div className="flex flex-wrap justify-center items-start mt-10 gap-6 xl:gap-0">
+      <div className="flex flex-col sm:flex-row sm:justify-between gap-3">
         {featureItems.map((feature, idx) => (
-          <FeatureElement feature={feature} key={idx} />
+          <FeatureElement feature={feature} key={idx}  />
         ))}
       </div>
     </div>
