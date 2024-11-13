2dd0115697ec30f34cb339984537eb33cedb03a1
hicass <hicass.dev@gmail.com>
2024-11-12 17:15:00 -0800

    Format code

--- client/components/LandingPage/DemoSection/DemoBlock/HighlightItem.jsx
+++ client/components/LandingPage/DemoSection/DemoBlock/HighlightItem.jsx
@@ -4,10 +4,8 @@ import { urlFor } from "@/src/sanity/lib/image";
 const HighlightItem = ({ text, icon, iconAlt }) => {
   return (
     <div className="flex gap-2">
-      <div>
-        {icon && <Image src={urlFor(icon).url()} alt={iconAlt} width={22} height={22} />}
-      </div>
-      
+      <div>{icon && <Image src={urlFor(icon).url()} alt={iconAlt} width={22} height={22} />}</div>
+
       <p className="body-txt">{text}</p>
     </div>
   );
