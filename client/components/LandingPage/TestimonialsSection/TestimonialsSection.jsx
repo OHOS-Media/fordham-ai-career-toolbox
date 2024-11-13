5ca0b5275bb11f731eacc62798f2703510ac71b4
Toni <pasotoni03@gmail.com>
2024-11-13 17:59:43 -0500

    Updated Testimonial Section: - Added header and change the color of it to white - Putted a border around text and align text

--- client/components/LandingPage/TestimonialsSection/TestimonialsSection.jsx
+++ client/components/LandingPage/TestimonialsSection/TestimonialsSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Testimonial = ({ quote, text, name, title }) => (
-  <div className="flex flex-col items-center text-center max-w-md mx-auto">
+  <div className="flex flex-col items-center text-left max-w-md mx-auto bg-white/5 p-6 rounded-lg h-full">
     <h3 className="text-2xl font-bold mb-6">&quot;{quote}&quot;</h3>
     <p className="text-sm mb-6 opacity-80">{text}</p>
-    <p className="font-semibold">{name}</p>
+    <p className="font-semibold mb-1">{name}</p>
     <p className="text-sm opacity-80">{title}</p>
   </div>
 );
@@ -16,11 +16,12 @@ const TestimonialsSection = ({ testimonials }) => {
   }
 
   return (
-    <section className="w-full text-white py-40 z-10">
+    <section className="w-full text-white pt-1 pb-20 z-10">
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap justify-center gap-20">
+        <h2 className="text-3xl font-bold text-center mb-10">User stories</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8">
           {testimonials.map((testimonial, index) => (
-            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
+            <div key={index} className="flex flex-col items-stretch text-center bg-white/5 rounded-lg" >
               <Testimonial {...testimonial} />
             </div>
           ))}
