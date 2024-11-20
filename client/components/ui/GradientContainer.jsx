5e5cb9534a10520d8ce23d91eb3d33ef114162cb
hicass <hicass.dev@gmail.com>
2024-11-20 11:46:10 -0800

    Update the Gradient container to use an png instead of an svg to preserve the blur quality

--- client/components/ui/GradientContainer.jsx
+++ client/components/ui/GradientContainer.jsx
@@ -3,7 +3,7 @@ export default function GradientContainer({ children }) {
     <div className="relative w-full">
       {/* Gradient Background */}
       <img
-        src="/assets/Gradient.svg"
+        src="/assets/Gradient.png"
         alt="Gradient background"
         className="absolute w-[700px] h-[700px] top-[-20%] left-[45%] pointer-events-none rotate-90"
         aria-hidden="true"
