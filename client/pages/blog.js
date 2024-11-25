62dd419b2aa9f1c798bf29ff97a0fb359e669772
Toni Paso <156366416+ToniPaso@users.noreply.github.com>
2024-11-25 15:54:41 -0500

    Updated responsive design in BlogsPreview.jsx and blog.js (#43)

--- client/pages/blog.js
+++ client/pages/blog.js
@@ -8,9 +8,9 @@ export default function Blog({ BlogPageData }) {
     <PageContainer marginBottom={true} marginTop={true}>
       <GradientContainer>
         <div className="flex flex-col gap-20">
-          <div className=" w-full flex flex-col items-center gap-2">
-            <h1 className="h2 text-fordham-white">RamResume blog</h1>
-            <p className="body-txt-md text-center font-light text-fordham-light-gray/60 max-w-2xl">
+          <div className=" w-full flex flex-col items-center gap-[16px] mt-[80px] mb-[80px] px-[40px]">
+            <h1 className="h3 text-fordham-white text-xl md:text-3xl lg:text-4xl">RamResume blog</h1>
+            <p className="body-txt-md text-center font-light text-fordham-light-gray/60 max-w-2xl text-base md:text-lg lg:text-xl">
               Your hub for all things job search and professional growth.
             </p>
           </div>
