dfb956346b87dd296726161e3a8c3a0e278b7241
Cass <hicass.dev@gmail.com>
2024-11-26 07:16:18 -0800

    feat: Update Mobile Nav and Add Animations to DemoBlock (#46)
    * Add slide animations to DemoBlock images
    
    * Update the Mobile Nav:
    - Update the design
    - Add animation
    - Remove unused imports and props
    
    * Remove unneeded code

--- client/components/Nav/MobileNav/MobileNav.jsx
+++ client/components/Nav/MobileNav/MobileNav.jsx
@@ -1,48 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavToggle from "./NavToggle";
-import Link from "next/link";
 import Logo from "@/components/ui/Logo";
 import MobileNavMenu from "./MobileNavMenu";
+import AuthNavButton from "../AuthNavButton";
 
-export default function MobileNav({
-  navData,
-  isAuthenticated,
-  login,
-  user,
-  error,
-  setError,
-  scrolled,
-}) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function MobileNav({ navData, login }) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const [atTop, setAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setAtTop(currentScrollPos < 10);
+      setVisible(
+        prevScrollPos > currentScrollPos || currentScrollPos < 10 || currentScrollPos < 100
+      );
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    if (!visible) {
+      setMenuIsOpen(false);
+    }
+  }, [visible]);
 
   return (
-    <nav
+    <header
       className={`
-      fixed top-0 left-0 right-0 z-50
-      transition-all duration-300
-      ${
-        isOpen
-          ? "bg-fordham-black h-screen"
-          : scrolled
-            ? "bg-fordham-black/95 backdrop-blur-sm"
-            : "bg-transparent"
-      }
+      w-full sticky top-0 z-50 pt-10
+      transition-all duration-500 transform px-5
+      ${visible ? "translate-y-0" : "-translate-y-full"}
     `}
     >
-      <div className="px-4 py-2 flex justify-between items-center">
-        <Logo />
-        <NavToggle isOpen={isOpen} setIsOpen={setIsOpen} />
-      </div>
+      <nav className="w-full max-w-5xl relative transition-all duration-300">
+        <div
+          className={`
+            h-[72px] mx-auto flex
+            justify-between items-center
+            pl-6 pr-3 rounded-2xl z-10
+          bg-white/5 backdrop-blur-[15px]
+            border-[1px] border-[#3B3533]
+          `}
+        >
+          <div className="flex items-center h-full" onClick={() => setMenuIsOpen(false)}>
+            <Logo />
+          </div>
 
-      {isOpen && (
-        <MobileNavMenu
-          navData={navData}
-          isAuthenticated={isAuthenticated}
-          login={login}
-          user={user}
-          setIsOpen={setIsOpen}
-        />
-      )}
-    </nav>
+          <div className="flex items-center gap-6 h-full">
+            <AuthNavButton onLogin={login} />
+
+            <NavToggle menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
+          </div>
+        </div>
+
+        {menuIsOpen && <MobileNavMenu navData={navData} setMenuIsOpen={setMenuIsOpen} />}
+      </nav>
+    </header>
   );
 }
