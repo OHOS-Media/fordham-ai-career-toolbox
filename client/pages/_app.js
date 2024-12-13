55fe933c8607818b59b897e8502b18470f642b55
Bach Le <lehoangbach7802@gmail.com>
2024-12-13 12:35:00 -0500

    feat: Integrate PostHog for analytics and clean up console logs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,11 +5,24 @@ import "@/styles/globals.css";
 import Layout from "@/components/Layout.jsx";
 import { client } from "@/src/sanity/lib/client.js";
 import { Toaster } from "react-hot-toast";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 const inter_init = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 export default function App({ Component, pageProps }) {
   const [footerData, setFooterData] = useState(null);
@@ -28,27 +41,29 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    <AuthProvider>
-      <main className={inter_init.className}>
-        <Layout footerData={footerData}>
-          <Component {...pageProps} />
-        </Layout>
-      </main>
+    <PostHogProvider client={posthog}>
+      <AuthProvider>
+        <main className={inter_init.className}>
+          <Layout footerData={footerData}>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
 
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          className: "",
-          duration: 3000,
-          style: {
-            background: "#FFFFFF",
-            opacity: 0.8,
-            color: "#282220",
-            borderRadius: "16px",
-            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)",
-          },
-        }}
-      />
-    </AuthProvider>
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            className: "",
+            duration: 3000,
+            style: {
+              background: "#FFFFFF",
+              opacity: 0.8,
+              color: "#282220",
+              borderRadius: "16px",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)",
+            },
+          }}
+        />
+      </AuthProvider>
+    </PostHogProvider>
   );
 }
